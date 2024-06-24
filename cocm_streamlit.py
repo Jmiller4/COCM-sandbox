1365fdbd3a4044b46cd564aff4952bca680a5a23
Joel Miller <joeldmiller73@gmail.com>
2024-06-24 14:35:38 -0500

    Update cocm_streamlit.py

--- cocm_streamlit.py
+++ cocm_streamlit.py
@@ -241,14 +241,17 @@ if 'iter' not in st.session_state:
 
 
 
-bc1, bc2, bcEmpty = st.columns(3)
+bc, bcEmpty1, bcEmpty2 = st.columns(3)
+
+bc1, bc2 = bc.columns(2)
 if bc1.button('Shuffle Donations'):
 	randomize_donations(num_donors, num_projects)
 	st.session_state.iter += 1
 if bc2.button('Clear Donations'):
 	clear_donations()
 	st.session_state.iter += 1
-bcEmpty = st.empty()
+bcEmpty1 = st.empty()
+bcEmpty2 = st.empty()
 
 
 
